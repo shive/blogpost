6281d9e797a2bec01b168a56af990a43fddf127c
shive <shive@users.noreply.github.com>
2013-09-21 01:19:54 +0900

    tornado

---
+++ 20130920-tornado/app.py
@@ -0,0 +1,13 @@
+﻿# -*- mode: python; coding: utf-8-with-signature-unix -*-
+#=======================================================================================================================
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!!'
+
+if __name__ == '__main__':
+    app.run()
+
