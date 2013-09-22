3be599a264fa1e1c5c407606976a795832577511
shive <shive@users.noreply.github.com>
2013-09-22 13:04:56 +0900

    tornado

--- 20130920-tornado/app.py
+++ 20130920-tornado/app.py
@@ -1,13 +1,27 @@
 ﻿# -*- mode: python; coding: utf-8-with-signature-unix -*-
 #=======================================================================================================================
 
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 app = Flask(__name__)
 
+
+#------------------------------------------------------------------------------
 @app.route('/')
 def hello():
     return 'Hello World!!'
 
+# #------------------------------------------------------------------------------
+# @app.route('/favicon.ico')
+# def favicon():
+#     app.logger.debug('app.root_path: %s' % app.root_path)
+#     return send_from_directory(
+#         os.path.join(app.root_path, 'static'),
+#         'favicon.ico',
+#         as_attachment = True,
+#         )
+
+#------------------------------------------------------------------------------
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
